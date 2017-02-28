a6f7195fe88f7d3da36f2bb393047872a73349a1
Fumio SAGAWA <fumio.sagawa@gmail.com>
2017-02-28 16:55:55 +0900

    first commit

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+//our root app component
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'src/app.html',
+  styleUrl: [''],
+})
+class App {
+  showStyle: false;
+  
+  constructor() {
+  }
+  
+  getStyle() {
+    if(this.showStyle){
+      return "yellow";
+    } else {
+      return "";
+    }
+  }
+}
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ App ],
+  bootstrap: [ App ]
+})
+export class AppModule {}
+
+ng2-co  
