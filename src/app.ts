aa0e489bbf0ef998a9b998d720ec17d3022b6736
Fumio SAGAWA <fumio.sagawa@gmail.com>
2017-03-04 11:10:26 +0900

    Built-in directives

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,49 @@
-//our root app component
-import { Component, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   template: `
-  <div>
-    <div [style.background-color]="getStyle()">
-      I'm a div that wants to be styled
-    </div>
-    <button (click)="showStyle = !showStyle;">Toggle style</button>
-  </div>
-  `
+<form #f="ngForm" novalidate>
+  <input type="text" name="hoge" ngModel required>
+  <p [hidden]="!f.form.dirty || f.form.valid">必須入力です</p>
+</form>
+<ul>
+  <li *ngFor="let data of demoData">{{data.name}} - {{data.age}}</li>
+</ul>
+  `,
+  styles:[`
+input.ng-invalid.ng-dirty {
+  border-color: #ff0000;
+}
+  }`]
 })
-class App {
-  showStyle: false;
-  
-  constructor() {
-  }
-  
-  getStyle() {
-    if(this.showStyle){
-      return "yellow";
-    } else {
-      return "";
-    }
-  }
+export class App implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+
+  demoData: any = [
+    {name: '山田', age: 24},
+    {name: '田中', age: 28},
+    {name: '佐藤', age: 18},
+    {name: '井上', age: 32},
+    {name: '高橋', age: 46}
+  ]
 }
 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ App ],
-  bootstrap: [ App ]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  exports: [],
+  declarations: [App],
+  providers: [],
+  bootstrap: [App]
 })
-export class AppModule {}
+export class AppModule { }
+
